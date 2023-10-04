4b202e2313ce9b16e469fa6f4cd0fb716290f32d
ikechukwu ugwuagu <ikechukwuugwuagu@gmail.com>
2023-10-04 10:52:46 +0100

    Payment intigration

---
+++ lib/inapp_payment/repositories/payment.repository.dart
@@ -0,0 +1,9 @@
+import 'package:pay/pay.dart';
+
+List<PaymentItem> paymentItems = [
+  const PaymentItem(
+    label: 'Order now',
+    amount: '5',
+    status: PaymentItemStatus.final_price,
+  ),
+];
